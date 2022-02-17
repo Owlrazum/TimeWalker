5822dfd99f202ab100e42de4f243ee8299e6f0e8
Owlrazum <owlrazum@gmail.com>
2022-02-17 13:04:52 +0600

    Added Approaching clock arrow Behaviour

--- Assets/_Game/Scripts/aGeneralTemplate/aUI/UIClockArrowController.cs
+++ Assets/_Game/Scripts/aGeneralTemplate/aUI/UIClockArrowController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,11 +9,22 @@ public class UIClockArrowController : MonoBehaviour, IPointerDownHandler, IDragH
 
     [SerializeField]
     [Tooltip("In degrees")]
-    private float speedOfTime;
+    private float speedOfTimeDeg;
+
+    [SerializeField]
+    private float speedOfApproachRad;
+
+    [SerializeField]
+    private float approachEpsilon;
 
     private RectTransform rect;
     private bool isPositive;
     private bool shouldTimePass;
+
+    private float currentAngle;
+
+    private float angleToApproach;
+    private bool haveApproached;
 
     private void Awake()
     {
@@ -20,53 +32,114 @@ public class UIClockArrowController : MonoBehaviour, IPointerDownHandler, IDragH
         timer = 0;
         isPositive = false;
         shouldTimePass = true;
+
+        currentAngle = 0;
     }
 
     private void Update()
     {
         if (shouldTimePass)
         { 
-            float step = speedOfTime * Time.deltaTime;
+            float step = speedOfTimeDeg * Time.deltaTime;
             arrowRectTransform.eulerAngles += new Vector3(0, 0, -step);
+            currentAngle = arrowRectTransform.eulerAngles.z * Mathf.Deg2Rad;
+
             EventsContainer.ClockArrowMoved?.Invoke(arrowRectTransform.eulerAngles.z * Mathf.Deg2Rad);
         }
     }
 
     public void OnPointerDown(PointerEventData data)
     {
-        OnDrag(data);
+        haveApproached = false;
         shouldTimePass = false;
+        OnDrag(data);
     }
 
     private float timer; 
     public void OnDrag(PointerEventData data)
     {
+        float angle = ComputePressAngle(data.position);
+
+        print("OnDrag");
+        if (!haveApproached)
+        {
+            print("Inside if");
+            angleToApproach = angle;
+            if (!isApproaching)
+            {
+                print("StartingCoroutine");
+                StartCoroutine(ApproachingCoroutine());
+            }
+            print("Returning");
+            return;
+        }
+        print(currentAngle + " : " + angle);
+        currentAngle = angle;
+        EventsContainer.ClockArrowMoved?.Invoke(currentAngle);
+        arrowRectTransform.eulerAngles = new Vector3(0, 0, currentAngle * Mathf.Rad2Deg);
+    }
+
+    private float ComputePressAngle(Vector3 pressPos)
+    { 
         Vector2 origin = Vector2.up;
-        Vector3 pressPos = data.position;
+
         if (!RectTransformUtility.RectangleContainsScreenPoint(rect, pressPos))
         {
             //return;
         }
-        Vector3 direction = pressPos - arrowRectTransform.position;
-        //Debug.DrawRay(Vector3.zero, direction * 10, Color.white, 10, false);
-        direction = Quaternion.Euler(0, 0, 90) * direction;
 
-        //Debug.DrawRay(Vector3.zero, direction * 10, Color.red, 10, false);
+        Vector3 pressDirection = pressPos - arrowRectTransform.position;
+        pressDirection = Quaternion.Euler(0, 0, 90) * pressDirection; // rotate for atan2
 
-
-        float angle = Mathf.Atan2(direction.y, -direction.x);
+        float angle = Mathf.Atan2(pressDirection.y, pressDirection.x);
+        angle += Mathf.PI;
         if (angle < 0)
         {
             angle = 2 * Mathf.PI + angle;
         }
+        
+        return angle;
+    }
 
-        EventsContainer.ClockArrowMoved?.Invoke(angle);
-        arrowRectTransform.eulerAngles = new Vector3(0, 0, -angle * Mathf.Rad2Deg);
+    private bool isApproaching;
+    private IEnumerator ApproachingCoroutine()
+    {
+        isApproaching = true;
+        while(!haveApproached)
+        {
+            print("Approaching");
+            ApproachTouchPos(angleToApproach);
+            yield return null;
+        }
+        isApproaching = false;
+    }
+
+    private void ApproachTouchPos(float angle)
+    {
+        float sign = 1;
+        if (currentAngle > angle)
+        {
+            sign = -1;
+        }
+        currentAngle += sign * speedOfApproachRad * Time.deltaTime;
+        if (sign > 0 && currentAngle > angle)
+        {
+            haveApproached = true;
+            currentAngle = angle;
+        }
+        else if (sign < 0 && currentAngle < angle)
+        {
+            haveApproached = true;
+            currentAngle = angle;
+        }
+        //print("cur : " + currentAngle + "\nappr " + angle);
+
+        EventsContainer.ClockArrowMoved?.Invoke(currentAngle);
+        arrowRectTransform.eulerAngles = new Vector3(0, 0, currentAngle * Mathf.Rad2Deg);
     }
 
     public void OnPointerUp(PointerEventData data)
     {
         shouldTimePass = true;
     }
-
 }
